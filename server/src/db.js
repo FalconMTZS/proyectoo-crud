b59e09b568e388a87cbe1de567dceb1619defc73
FalconMTZS <l23590205@sjuanrio.tecnm.mx>
2026-05-24 13:27:25 -0600

    Conexión definitiva usando pooler y SSL nativo

--- server/src/db.js
+++ server/src/db.js
@@ -4,14 +4,13 @@ import 'dotenv/config';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true // Esto obliga a usar la conexión segura nativa que pide el pooler de Neon
 });
 
+// Prueba de conexión inicial
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
-    console.error('❌ Error de conexión con el Postgres de Neon:', err.stack);
+    console.error('❌ Error de conexión con el Postgres de Neon:', err.message);
   } else {
     console.log('⚡ ¡Espectacular! Servidor conectado exitosamente a Neon en la nube.');
   }
